7fbdcb6c583bd0178d2c1a20046065827e7a01b5
Adriano Tadao <drianotadao@gmail.com>
2014-10-07 17:59:27 -0300

    Initial Commig

---
+++ config/files.js
@@ -0,0 +1,28 @@
+/* Exports a function which returns an object that overrides the default &
+ *   plugin file patterns (used widely through the app configuration)
+ *
+ * To see the default definitions for Lineman's file paths and globs, see:
+ *
+ *   - https://github.com/linemanjs/lineman/blob/master/config/files.coffee
+ */
+
+module.exports = function(lineman) {
+  return {
+    js: {
+      vendor: [
+        "vendor/js/jquery/dist/jquery.js",
+        "vendor/js/underscore/underscore.js",
+        "vendor/js/backbone/backbone.js",
+        "vendor/js/backbone.babysitter/lib/backbone.babysitter.js",
+        "vendor/js/backbone.wreqres/lib/backbone.wreqres.js",
+        "vendor/js/backbone.wreqres/lib/backbone.wreqres.js",
+        "vendor/js/marionette/lib/backbone.marionette.js",
+        "vendor/js/boostrap/dist/js/boostrap.js"
+      ]
+    },
+
+    sass: {
+      main: 'app/css/*.{scss,sass}'
+    }
+  };
+};
